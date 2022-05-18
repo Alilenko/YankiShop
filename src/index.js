e5cb51bc1b14865ace9c22cdca32669c0e4615dd
Alina <pavlenkoalina9694@gmail.com>
2022-05-18 16:50:27 +0300

    added gh-pages

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React, {Suspense} from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import './i18next';
-import {BrowserRouter as Router} from 'react-router-dom'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import "./i18next";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './styles/index.scss';
+import "./styles/index.scss";
 
 ReactDOM.render(
-  <Suspense fallback={(<div>Loading...</div>)}>
+  <Suspense fallback={<div>Loading...</div>}>
     <Provider store={store}>
-      <Router>
+      <Router basename="/YankiShop">
         <App />
       </Router>
     </Provider>
   </Suspense>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
